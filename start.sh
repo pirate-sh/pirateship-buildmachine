92eb7834351529607a4622fe5c7a5c595741b29a
dogi <dogi@users.noreply.github.com>
2016-04-07 02:35:08 -0400

    some tweaks

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#vagrant -h || die echo "please install vagrant: http://vagrantup.com""
-
-
-mkdir -p download
-cd download
-wget -c http://director.downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2016-03-18/2016-03-18-raspbian-jessie-lite.zip
+wget --directory-prefix download -c http://director.downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2016-03-18/2016-03-18-raspbian-jessie-lite.zip
 
 vagrant up
 vagrant ssh -c /vagrant/onboot.sh
